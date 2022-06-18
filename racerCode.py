a5e250f138a6a649277032ab7500bd96ef3d4260
phil-qa <104486392+phil-qa@users.noreply.github.com>
2022-06-18 17:17:58 +0100

    Implementing angle calculations

--- racerCode.py
+++ racerCode.py
@@ -15,21 +15,29 @@ implement will_hit_target - iteration 1:  will the pod be facing the target by t
 tich and the turn rate is 17 degrees a tick the algorithm would be the distance to the target and the starting speed and angle
 add additional telemetry to determine turn rate is it constant - NO
 add code to decelerate and accelerate - Done
-Implement trajectorey angle as simple response
+Implement trajectorey angle as simple response - done
+implement apex points
 '''
 
 def debug(m):
     print(m, file=sys.stderr, flush=True)
 
-def calculate_distance(reference_x, reference_y, target_x, target_y):
+def distance_between_two_points(reference_x, reference_y, target_x, target_y):
     x_delta = target_x - reference_x
     y_delta = target_y - reference_y
     return int(math.sqrt((x_delta ** 2) + (y_delta ** 2)))
 
+def calculate_angles_from_coords(point_a.x, point_a.y, point_b.x, point_b.y, point_c.x, point_c.y):
+    distance_a_to_b = distance_between_two_points(point_a.x, point_a.y, point_b.x, point_b.y)
+    distance_b_to_c = distance_between_two_points(point_b.x, point_b.y, point_c.x, point_c.y)
+    distance_c_to_a = distance_between_two_points(point_c.x, point_c.y, point_a.x, point_a.y)
+    
+
 def calculate_trajectory(primary_x, primary_y, secondary_x, secondary_y, target_x, target_y):
-    reference_line = calculate_distance(primary_x, primary_y, target_x, target_y)
-    travel_line = calculate_distance(primary_x, primary_y, secondary_x, secondary_y)
-    target_line = calculate_distance(secondary_x,secondary_y, target_x, target_y)
+    reference_line = distance_between_two_points(primary_x, primary_y, target_x, target_y)
+    travel_line = distance_between_two_points(primary_x, primary_y, secondary_x, secondary_y)
+    target_line = distance_between_two_points(secondary_x,secondary_y, target_x, target_y)
+
     reference_angle = int(57.29*((acos((reference_line**2+travel_line**2-target_line**2)/(2*reference_line * travel_line)).real)))
     travel_angle = int(57.29*(acos((travel_line**2 + target_line **2 - reference_line **2)/ (2 * travel_line * target_line)).real))
     target_angle =int( 57.29*(acos ((target_line ** 2 + reference_line ** 2 - travel_line ** 2)/(2 * target_line * reference_line)).real))
@@ -54,12 +62,23 @@ class Checkpoint:
         self.x = x
         self.y = y
         self.distance_to_next = None
+        self.apex_x = None
+        self.apex_y = None 
+        self.angle_to_next = None
 
     def __str__(self):
         return(f"{self.position}, {self.x}, {self.y}, {self.distance_to_next}")
 
     def update_distance_to_next(self, next_x, next_y):
-        self.distance_to_next = calculate_distance(self.x,self.y, next_x, next_y)
+        self.distance_to_next = distance_between_two_points(self.x,self.y, next_x, next_y)
+    
+    def set_angle_to_next(self, previous_checkpoint, next_checkpoint):
+        trajectory_data = calculate_trajectory(previous_checkpoint.x, previous_checkpoint.y,self.x, self.y,next_checkpoint.x, next_checkpoint.y)
+        self.angle_to_next = trajectory_data[0]
+    
+
+
+     
 
 class Telemetry:
     def __init__(self):
@@ -113,16 +132,26 @@ class Course:
             for c in self.checkpoints:
                 debug(c)
     
-        elif self.is_map_conditions_met(next_checkpoint_x, next_checkpoint_y):
+        elif self.is_map_conditions_met(next_checkpoint_x, next_checkpoint_y): #set checkpoints
             debug("Map read executing analysis")
             for c_index in range(len(self.checkpoints)):
+                wrap_index = c_index+1 if c_index+1 < len(self.checkpoints) else -1
                 set_distance(self.checkpoints[c_index - 1], self.checkpoints[c_index])
+                debug(f"{c_index}, {wrap_index}")
+                self.checkpoints[c_index].set_angle_to_next(self.checkpoints[c_index-1], self.checkpoints[wrap_index])
+
             for c in self.checkpoints:
                 debug(c)
             self.course_known = True
 
+def map_checkpoint(previous_checkpoint, current_checkpoint, next_checkpoint):
+    current_checkpoint.update_distance_to_next(next_checkpoint.x, next_checkpoint.y)
+    current_checkpoint.apex_angle = calculate_trajectory(previous_checkpoint.x, previous_checkpoint.y, current_checkpoint.x, current_checkpoint.y, next_checkpoint.x, next_checkpoint.y)[0]
 
-    
+
+def set_distance(source_checkpoint, target_checkpoint):
+       source_checkpoint.update_distance_to_next(target_checkpoint.x, target_checkpoint.y)
+  
     
         
 
@@ -167,7 +196,7 @@ class Pod:
     def update_speed(self, new_x, new_y):
         self.x_momentum = (self.x - new_x)*-1
         self.y_momentum = (self.y - new_y)*-1
-        self.speed = calculate_distance(self.x,self.y,new_x,new_y)
+        self.speed = distance_between_two_points(self.x,self.y,new_x,new_y)
     
     def update_pod_position(self, new_x, new_y):
         self.x = new_x
@@ -192,8 +221,6 @@ class Pod:
         return(f"pod {self.identity} Speed {self.speed} Speed to target {self.speed_to_target}, trajectory angle to target {self.trajectory_angle}")
 
 
-def set_distance(source_checkpoint, target_checkpoint):
-     source_checkpoint.update_distance_to_next(target_checkpoint.x, target_checkpoint.y)
 
 
 
@@ -255,7 +282,7 @@ while True:
         lengths.insert(0, lengths.pop())
         debug(f"{lengths}, {lengths.index(max(lengths))+1}" )
 
-        player.decellerate(40) if not player.hit_next_target else player.accellerate(50)
+        player.decellerate(40) if not player.hit_next_target else player.accellerate(60)
 
         longest_leg = next(x for x in checkpoints if x.distance_to_next == max(lengths))
         debug(longest_leg.position)
@@ -266,10 +293,7 @@ while True:
                 player.fire_boost = True
                 boost_fired = True
     else:
-        if player.trajectory_angle > 40:
-            player.decellerate(5)
-        else:
-            player.accellerate(30)
+        player.decellerate(40) if not player.hit_next_target else player.accellerate(60)
 
 
 
